08ae525d86960e994c050e8c6d2e2724490ac6cf
Santhosh <shanmugamsanthosh22@gmail.com>
2023-12-13 02:34:08 +0530

    first commit

--- src/components/dashboard/ApiHubs.jsx
+++ src/components/dashboard/ApiHubs.jsx
@@ -18,7 +18,7 @@ function ApiHubs() {
     setLoading(true);
     console.log(updatedApi)
     try {
-      const response = await axios.get(`${baseUrl}/getallapis`, {
+      const response = await axios.get(`https://santechapi-backend.vercel.app/getallapis`, {
         headers: {
           'token': user.accessToken,
         },
