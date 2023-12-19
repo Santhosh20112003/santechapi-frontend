7ee9851076ccd4b49483ab4e0170459611aef533
Santhosh <shanmugamsanthosh22@gmail.com>
2023-12-19 19:15:16 +0530

    Final

--- src/components/home/App.jsx
+++ src/components/home/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import logo from "../assert/mockup-api.png";
 import Code from "./code";
 import CodeSnippet from "./CodeSnippet";
@@ -11,16 +12,36 @@ import Notificationbanner from "./notificationbanner";
 import Pricing from "./pricing";
 import Steps from "./steps";
 import Testimonials from "./testimonials";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
+  useEffect(() => {
+    function handleOffline() {
+      toast.error("You're Offline",{
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      })
+    }
+    window.addEventListener('offline', handleOffline);
+    return () => {
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
+
   return (
     <div className="bg-[url(https://ik.imagekit.io/iete/assets/back.png)] bg-contain">
     <Notificationbanner/>
     <Header/>
     <Hero/>
     <CodeSnippet/>
-    
     <Code/>
     <List/>
     <Steps/>
@@ -28,6 +49,7 @@ function App() {
     <Pricing/>
     <Contact/>
     <Footer/>
+    <ToastContainer/>
     </ div>
   );
 }
